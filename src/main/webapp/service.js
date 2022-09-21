086f1afc56cce6e6d19a28a6117bfb3fd7ce12e1
kellyvdb <kellyvdb@users.noreply.github.com>
2022-09-21 14:55:28 +0200

    hallo

--- src/main/webapp/service.js
+++ src/main/webapp/service.js
@@ -1,6 +1,7 @@
-// export default class service {
-//     static fetch() {
-//         return fetch('/subsidie/')
-//             .then((response) => response.json());
-//     }
-// }
+export default class service {
+    static fetch() {
+        return fetch('/subsidie/')
+            .then((response) => response.json())
+            .then(console.log(response.json()));
+    }
+}
