627a4352eea0f6da8990e3e10ecc2a143848c868
Ayoub A <38209147+Ayoub2K@users.noreply.github.com>
2022-09-30 15:33:09 +0200

    kapote push

--- src/main/java/com/example/subsidieradar/SubsidieRadarApplication.java
+++ src/main/java/com/example/subsidieradar/SubsidieRadarApplication.java
@@ -6,6 +6,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SubsidieRadarApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SubsidieRadarApplication.class, args);
-	}
+		SpringApplication.run(SubsidieR
 }
