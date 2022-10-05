51e8711d5de620d60980568bd4bb804e8e14c003
kellyvdb <kellyvdb@users.noreply.github.com>
2022-10-05 12:38:51 +0200

    test

--- src/main/java/com/example/subsidieradar/domain/iMatch.java
+++ src/main/java/com/example/subsidieradar/domain/iMatch.java
@@ -3,5 +3,5 @@ package com.example.subsidieradar.domain;
 import java.util.List;
 
 public interface iMatch {
-    public List<Subsidie> MatchSubsidies(Project project);
+    public List<Subsidie> findMatches(Project project);
 }
