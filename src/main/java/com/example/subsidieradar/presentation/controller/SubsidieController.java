dc5051590c5a569f137913143fbfc314a501facb
Ayoub A <38209147+Ayoub2K@users.noreply.github.com>
2022-10-19 11:19:35 +0200

    toevoegen controllers

--- src/main/java/com/example/subsidieradar/presentation/controller/SubsidieController.java
+++ src/main/java/com/example/subsidieradar/presentation/controller/SubsidieController.java
@@ -1,5 +1,6 @@
 package com.example.subsidieradar.presentation.controller;
 
+import com.example.subsidieradar.application.SubsidieService;
 import com.example.subsidieradar.presentation.dto.SubsidieDTO;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,10 +8,15 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:8080/")
 @RequestMapping("/subsidie")
 public class SubsidieController {
+    private final SubsidieService subsidieService;
+
+    public SubsidieController(SubsidieService subsidieService) {
+        this.subsidieService = subsidieService;
+    }
 
     @GetMapping
-    public SubsidieDTO Hello() {
-        return new SubsidieDTO("hello world");
+    public SubsidieDTO getSubsidies() throws Exception {
+        return (SubsidieDTO) subsidieService.getSubsidies();
     }
 
 //    @GetMapping
