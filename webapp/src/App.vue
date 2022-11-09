6b9a7ca382fd04577ebbe2f57479cd1a4b4f962b
kellyvdb <kellyvdb@users.noreply.github.com>
2022-11-09 12:34:18 +0100

    frontend subsidieoverzicht

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/subsidie">Subsidies</router-link> |
+    <router-link to="/subsidie">Subsidie</router-link> |
   </div>
   <router-view/>
 </template>
--- webapp/src/views/Subsidie.vue
+++ webapp/src/views/Subsidie.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="container">
-    <h1>SubsidieLijst</h1>
-    <table>
+    <h1 class="text-center"> Subsidie Lijst</h1>
+    <table class="table table-striped">
       <thead>
-        <th>Subsidie naam</th>
+      <th>
+        <input type="checkbox" v-model="allSelected" @change="selectAll"></th>
+      <th>Subsidie naam</th>
+      <th>beoordelingssystematiek</th>
+      <th>Deadline</th>
+      <th>budget</th>
       </thead>
       <tbody>
-          <td>{{subsidieNaam}}</td>
+      <tr v-for="subsidie in subsidies" :key="subsidie.id">
+        <td><input type="checkbox" v-model="selected" :value="subsidie.id"></td>
+
+        <td>{{subsidie.naam}}</td>
+        <td>{{subsidie.beoordelingssystematiek}}</td>
+        <td>{{subsidie.deadline}}</td>
+        <td>{{subsidie.subsidieBudget}}</td>
+      </tr>
       </tbody>
+
     </table>
   </div>
 </template>
@@ -15,24 +28,34 @@
 <script>
 import SubsidieService from "@/services/SubsidieService";
 
-export default {
-  name: "subsidie-item",
-  data(){
-    return{
-      subsidies : []
-    }
+export default{
+  name:'Subsidie-items',
+  data() {
+    return {
+      subsidies:[],
+    };
   },
   methods:{
-    getSubsidie(){
-      SubsidieService.getSubsidie().then((response) => {
-        //TODO: gaat iets mis met deze return items zijn leeg
-        console.log(this.subsidies)
-        this.subsidies = response.data
-      })
+    getSubsidies(){
+      SubsidieService.getSubsidies().then(
+          response => {
+            this.subsidies = response.data;
+          }
+      );
+    },
+    selectAll(){
+      this.employeeIds =[];
+      if(this.allSelected){
+        const selected = this.subsidies.map((subsidie)=>subsidie.id)
+        this.selected = selected;
+
+      }else{
+        this.selected=[];
+      }
     }
   },
   created(){
-    this.getSubsidie()
+    this.getSubsidies()
   }
 }
 </script>
