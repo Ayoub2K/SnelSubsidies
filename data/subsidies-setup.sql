962d53f9d691506c16b2919c00234f99fe79d5ff
Ayoub A <38209147+Ayoub2K@users.noreply.github.com>
2022-11-12 11:51:04 +0100

    fixed sql

--- data/subsidies-setup.sql
+++ data/subsidies-setup.sql
@@ -10,7 +10,7 @@ create table subsidie
     themas varchar(125),
     subsidiabele_activiteiten varchar(1000),
     locatie  varchar(125),
-    soort_organisatie varchar(125),
+    soort_organisatie varchar(300),
     samenwerking varchar(1000),
     type_samenwerking varchar(1000),
     totaal_budget int,
@@ -22,10 +22,12 @@ create table subsidie
     datum_open varchar(20),
     datum_sluit varchar(20),
     beoordeling_tender varchar(125),
-    looptijdProject_jaren int
+    looptijdProject_jaren decimal
 );
 
+
+
 COPY subsidie
-    FROM program 'cut -d ";" -f 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21 /var/data/db/CityDealsData/new.csv'
-    DELIMITER ';'
+    FROM program 'cut -d "," -f 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21 /var/data/db/CityDealsData/Circulair-Bouwen.csv'
+    DELIMITER ','
     CSV HEADER;
