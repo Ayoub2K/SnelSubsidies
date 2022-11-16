097ec47ddd1c676261aff6d3f2252bf0aa2632ce
Ayoub A <38209147+Ayoub2K@users.noreply.github.com>
2022-11-16 11:40:10 +0100

    update form

--- webapp/src/views/Form.vue
+++ webapp/src/views/Form.vue
@@ -1,7 +1,8 @@
 <template>
   <form @submit.prevent="submit">
-    <div>In welke sector werkt u?: {{ sector }}</div>
 
+    <!--    sector-->
+    <div>In welke sector werkt u?: {{ sector }}</div>
     <select v-model="sector">
       <option disabled value="">sector</option>
       <option>A</option>
@@ -9,11 +10,38 @@
       <option>C</option>
     </select>
 
+    <!--    themas-->
+    <div>onder welke thema valt uw project?: {{ thema }}</div>
+    <select v-model="thema">
+      <option disabled value="">thema</option>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+
+    <!--    type activiteit-->
+    <div>wat is het type van uw activiteit?: {{ typeActiviteit }}</div>
+    <select v-model="typeActiviteit">
+      <option disabled value="">type activiteit</option>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+
+    <!--    minimale budget bedrag-->
     <p>wat is het minimaal benodigd subsidiebedrag?: €{{ budget }}</p>
     €<input v-model="budget" placeholder="0"/>
 
-    <div>wat is de projectlocatie?: {{ projectlocatie }}</div>
+    <!--    startdatum-->
+    <p>wat is uw beoogde startdatum?: {{ startDatum }}</p>
+    <input v-model="startDatum" placeholder="01-01-2000"/>
 
+    <!--    einddatum-->
+    <p>wat is uw beoogde einddatum?: {{ eindDatum }}</p>
+    <input v-model="eindDatum" placeholder="01-01-2000"/>
+
+    <!-- projectlocatie -->
+    <div>wat is de projectlocatie?: {{ projectlocatie }}</div>
     <select v-model="projectlocatie">
       <option disabled value="">projectlocatie</option>
       <option>A</option>
@@ -21,20 +49,19 @@
       <option>C</option>
     </select>
 
-    <p>datum is: {{ datum }}</p>
-    <input v-model="datum" placeholder="01-01-2000"/>
+    <!--    bijdrage-->
+    <p>kunt u cofinancieren? : €{{ bijdrage }}</p>
+    <input type="checkbox" id="checkbox" v-model="bijdrage" />
+    <label for="checkbox">{{ bijdrage }}</label>
 
-    <div>Selecteer de meest relevant onderwerpen?: {{ onderwerpen }}</div>
-
-    <select v-model="onderwerpen" multiple>
+    <!--    type samenwerking-->
+    <div>wat is het type samenwerking?: {{ samenwerking }}</div>
+    <select v-model="samenwerking">
+      <option disabled value="">type samenwerking</option>
       <option>A</option>
       <option>B</option>
       <option>C</option>
-      <option>D</option>
     </select>
-
-    <p>wat is uw maximale bijdrage?: €{{ bijdrage }}</p>
-    €<input v-model="bijdrage" placeholder="0"/>
 
     <br/>
     <br/>
@@ -54,11 +81,14 @@ export default {
   data() {
     return {
       sector: [],
+      thema: [],
+      typeActiviteit: [],
       budget: '',
+      startDatum: '',
+      eindDatum: '',
       projectlocatie: [],
-      datum: '',
-      onderwerpen: [],
-      bijdrage: ''
+      bijdrage: false,
+      samenwerking: []
     }
   }
   ,
