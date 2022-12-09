6084641d1c13b8b78a1023f50808d55adb5aeb8c
Ayoub A <38209147+Ayoub2K@users.noreply.github.com>
2022-12-09 10:45:51 +0100

    Update StepFormComponent.vue

--- webapp/src/components/StepFormComponent.vue
+++ webapp/src/components/StepFormComponent.vue
@@ -1,21 +1,16 @@
 <template>
-  <form @submit.prevent="submit">
+  <form @submit.prevent="submit" class="vragenForm">
     <section v-if="step === 1">
-      <h3>1. In welke sector werkt u?: {{ sector }}</h3>
+      <h3>1. In welke sector werkt u?</h3>
       <select v-model="sector" class="select">
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
-        <option value="4">Four</option>
-        <option value="5">Five</option>
-        <option value="6">Six</option>
-        <option value="7">Seven</option>
-        <option value="8">Eight</option>
+        <option value="1">data-leeg</option>
+        <option value="2">data-leeg</option>
+        <option value="3">data-leeg</option>
       </select>
     </section>
 
     <section v-if="step === 2">
-      <h3>2. onder welke thema valt uw project?: {{ thema }}</h3>
+      <h3>2. onder welke thema valt uw project?</h3>
       <select v-model="thema">
         <option disabled value="">thema</option>
         <option>Circulair bouwen</option>
@@ -35,7 +30,7 @@
     </section>
 
     <section v-if="step === 3">
-      <h3>3. wat is het type van uw activiteit?: {{ typeActiviteit }}</h3>
+      <h3>3. wat is het type van uw activiteit?</h3>
       <select v-model="typeActiviteit">
         <option disabled value="">type activiteit</option>
         <option>A</option>
@@ -45,22 +40,22 @@
     </section>
 
     <section v-if="step === 4">
-      <h3>4. wat is het minimaal benodigd subsidiebedrag?: €{{ budget }}</h3>
+      <h3>4. wat is het minimaal benodigd subsidiebedrag?</h3>
       €<input v-model="budget" placeholder="0"/>
     </section>
 
     <section v-if="step === 5">
-      <h3>5. wat is uw beoogde startdatum?: {{ startDatum }}</h3>
+      <h3>5. wat is uw beoogde startdatum?</h3>
       <input type="date" v-model="startDatum" min="2022-01-01">
     </section>
 
     <section v-if="step === 6">
-      <h3>6. wat is uw beoogde einddatum?: {{ eindDatum }}</h3>
+      <h3>6. wat is uw beoogde einddatum?</h3>
       <input type="date" v-model="eindDatum" min="2022-01-01">
     </section>
 
     <section v-if="step === 7">
-      <h3>7. wat is de projectlocatie?: {{ projectlocatie }}</h3>
+      <h3>7. wat is de projectlocatie?</h3>
       <select v-model="projectlocatie">
         <option disabled value="">projectlocatie</option>
         <option>Gelderland</option>
@@ -70,13 +65,13 @@
     </section>
 
     <section v-if="step === 8">
-      <h3>8. kunt u cofinancieren? : {{ bijdrage }}</h3>
+      <h3>8. kunt u cofinancieren?</h3>
       <input type="checkbox" id="checkbox" v-model="bijdrage"/>
       <label for="checkbox">{{ bijdrage }}</label>
     </section>
 
     <section v-if="step === 9">
-      <h3>9. wat is het type samenwerking?: {{ samenwerking }}</h3>
+      <h3>9. wat is het type samenwerking?</h3>
       <select v-model="samenwerking">
         <option disabled value="">type samenwerking</option>
         <option>Nee</option>
@@ -91,8 +86,11 @@
     </section>
     <br/>
 
-    <button class="button btn-prev" v-if="step !== 1" @click.prevent="prevStep">Vorige Stap</button>
-    <button class="button btn-next"  v-if="step !== totalsteps" @click.prevent="nextStep">Volgende Stap</button>
+    <div class="buttons">
+      <button class="button btn-prev" v-if="step !== 1" @click.prevent="prevStep">Vorige Stap</button>
+      <button class="button btn-skip" v-if="step !== totalsteps" @click.prevent="nextStep">Overslaan</button>
+      <button class="button btn-next" v-if="step !== totalsteps" @click.prevent="nextStep">Volgende Stap</button>
+    </div>
 
   </form>
 </template>
@@ -115,22 +113,22 @@ export default {
       samenwerking: []
     }
   },
-  methods:{
-    setstorage(){
-      sessionStorage.setItem('sector',JSON.stringify(this.sector))
-      sessionStorage.setItem('thema',JSON.stringify(this.thema))
-      sessionStorage.setItem('typeActiviteit',JSON.stringify(this.typeActiviteit))
-      sessionStorage.setItem('budget',JSON.stringify(this.budget))
-      sessionStorage.setItem('startDatum',JSON.stringify(this.startDatum))
-      sessionStorage.setItem('eindDatum',JSON.stringify(this.eindDatum))
-      sessionStorage.setItem('projectlocatie',JSON.stringify(this.projectlocatie))
-      sessionStorage.setItem('bijdrage',JSON.stringify(this.bijdrage))
-      sessionStorage.setItem('samenwerking',JSON.stringify(this.samenwerking))
+  methods: {
+    setstorage() {
+      sessionStorage.setItem('sector', JSON.stringify(this.sector))
+      sessionStorage.setItem('thema', JSON.stringify(this.thema))
+      sessionStorage.setItem('typeActiviteit', JSON.stringify(this.typeActiviteit))
+      sessionStorage.setItem('budget', JSON.stringify(this.budget))
+      sessionStorage.setItem('startDatum', JSON.stringify(this.startDatum))
+      sessionStorage.setItem('eindDatum', JSON.stringify(this.eindDatum))
+      sessionStorage.setItem('projectlocatie', JSON.stringify(this.projectlocatie))
+      sessionStorage.setItem('bijdrage', JSON.stringify(this.bijdrage))
+      sessionStorage.setItem('samenwerking', JSON.stringify(this.samenwerking))
     },
-    nextStep:function (){
+    nextStep: function () {
       this.step++;
     },
-    prevStep:function (){
+    prevStep: function () {
       this.step--;
     },
     submit() {
@@ -143,6 +141,22 @@ export default {
 }
 </script>
 <style scoped>
+.buttons{
+  text-align: center;
+}
+.vragenForm {
+  padding-top: 5%;
+  width: 100%;
+}
+
+h3 {
+  text-align: center;
+  font-size: 25px;
+}
+
+section {
+  text-align: center;
+}
 
 .button {
   border: none;
@@ -152,15 +166,16 @@ export default {
   text-decoration: none;
   display: inline-block;
   font-size: 16px;
+  margin: 0 10px;
 }
 
 .btn-prev {
-  background-color: #ababab;
+  background-color: #e0933a;
 }
 
 .btn-prev:hover {
-  cursor:pointer;
-  background-color: #cecece;
+  cursor: pointer;
+  background-color: #9d6e39;
 }
 
 .btn-next {
@@ -168,8 +183,17 @@ export default {
 }
 
 .btn-next:hover {
-  cursor:pointer;
+  cursor: pointer;
   background-color: #89db8e;
+}
+
+.btn-skip {
+  background-color: #ababab;
+}
+
+.btn-skip:hover {
+  cursor: pointer;
+  background-color: #cecece;
 }
 
 </style>
