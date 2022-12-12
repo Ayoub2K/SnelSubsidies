faccf0a2d0fd262be996b4c5311d141c44ee98d0
kellyvdb <kellyvdb@users.noreply.github.com>
2022-12-12 16:56:25 +0100

    info button toegevoegd

--- webapp/src/components/StepFormComponent.vue
+++ webapp/src/components/StepFormComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <form @submit.prevent="submit" class="vragenForm">
     <section v-if="step === 1">
-      <h3>1. In welke sector werkt u? <img id="info-icon" alt="Open link" src="../assets/info_icon.png"> </h3>
+      <h3>1. In welke sector werkt u? <img class="info-icon" alt="Open link" src="../assets/info_icon.png"> </h3>
       <select v-model="sector" class="select">
         <option value="1">data-leeg</option>
         <option value="2">data-leeg</option>
@@ -10,7 +10,7 @@
     </section>
 
     <section v-if="step === 2">
-      <h3>2. Onder welke thema valt uw project?</h3>
+      <h3>2. Onder welke thema valt uw project? <img class="info-icon" alt="Open link" src="../assets/info_icon.png"> </h3>
       <select v-model="thema">
         <option disabled value="">tThema</option>
         <option>Circulair bouwen</option>
@@ -29,7 +29,7 @@
     </section>
 
     <section v-if="step === 3">
-      <h3>3. Wat is het type van uw activiteit?</h3>
+      <h3>3. Wat is het type van uw activiteit? <img class="info-icon" alt="Open link" src="../assets/info_icon.png"> </h3>
       <select v-model="typeActiviteit">
         <option disabled value="">Type activiteit</option>
         <option>A</option>
@@ -39,22 +39,22 @@
     </section>
 
     <section v-if="step === 4">
-      <h3>4. Wat is het minimaal benodigd subsidiebedrag?</h3>
+      <h3>4. Wat is het minimaal benodigd subsidiebedrag? <img class="info-icon" alt="Open link" src="../assets/info_icon.png"> </h3>
       €<input v-model="budget" placeholder="0"/>
     </section>
 
     <section v-if="step === 5">
-      <h3>5. Wat is uw beoogde startdatum?</h3>
+      <h3>5. Wat is uw beoogde startdatum? <img class="info-icon" alt="Open link" src="../assets/info_icon.png"> </h3>
       <input type="date" v-model="startDatum" min="2022-01-01">
     </section>
 
     <section v-if="step === 6">
-      <h3>6. Wat is uw beoogde einddatum?</h3>
+      <h3>6. Wat is uw beoogde einddatum? <img class="info-icon" alt="Open link" src="../assets/info_icon.png"> </h3>
       <input type="date" v-model="eindDatum" min="2022-01-01">
     </section>
 
     <section v-if="step === 7">
-      <h3>7. Wat is de projectlocatie?</h3>
+      <h3>7. Wat is de projectlocatie? <img class="info-icon" alt="Open link" src="../assets/info_icon.png"> </h3>
       <select v-model="projectlocatie">
         <option disabled value="">projectlocatie</option>
         <option>Gelderland</option>
@@ -64,13 +64,13 @@
     </section>
 
     <section v-if="step === 8">
-      <h3>8. Kunt u cofinancieren?</h3>
+      <h3>8. Kunt u cofinancieren? <img class="info-icon" alt="Open link" src="../assets/info_icon.png"> </h3>
       <input type="checkbox" id="checkbox" v-model="bijdrage"/>
       <label for="checkbox">{{ bijdrage }}</label>
     </section>
 
     <section v-if="step === 9">
-      <h3>9. Wat is het type samenwerking?</h3>
+      <h3>9. Wat is het type samenwerking? <img class="info-icon" alt="Open link" src="../assets/info_icon.png"> </h3>
       <select v-model="samenwerking">
         <option disabled value="">type samenwerking</option>
         <option>Nee</option>
@@ -148,7 +148,7 @@ export default {
   width: 100%;
 }
 
-#info-icon {
+.info-icon {
   width: 1%;
   margin-left: 1%;
   cursor: pointer;
